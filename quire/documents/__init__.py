8edbe3ec7b217e59855a8fe97863de90e01dac6f
Christopher Toth <q.alpha@gmail.com>
2026-04-18 16:14:33 -0600

    Export document codec helpers

--- quire/documents/__init__.py
+++ quire/documents/__init__.py
@@ -10,6 +10,16 @@ from quire.documents.schema import (
     load_document_dir,
     to_document_builtins,
 )
+from quire.documents.codecs import (
+    decode_document,
+    decode_yaml_mapping,
+    decode_yaml_value,
+    document_to_payload,
+    encode_document,
+    encode_yaml_value,
+    render_document,
+    render_yaml_value,
+)
 from quire.documents.loaded import LoadedDocument
 
 __all__ = [
@@ -17,9 +27,17 @@ __all__ = [
     "DocumentStruct",
     "LoadedDocument",
     "convert_document_value",
+    "decode_document",
     "decode_document_bytes",
     "decode_document_path",
+    "decode_yaml_mapping",
+    "decode_yaml_value",
+    "document_to_payload",
+    "encode_document",
+    "encode_yaml_value",
     "load_document",
     "load_document_dir",
+    "render_document",
+    "render_yaml_value",
     "to_document_builtins",
 ]
