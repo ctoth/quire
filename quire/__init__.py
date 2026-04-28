f469665c9a1fe234780917a109ae2a65bdfcdc6f
Christopher Toth <q.alpha@gmail.com>
2026-04-28 00:55:44 -0600

    Export remaining propstore quire imports

--- quire/__init__.py
+++ quire/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from quire import documents
 from quire.artifacts import (
     ArtifactFamily,
+    ArtifactHandle,
     BranchPlacement,
     FixedFilePlacement,
     FlatYamlPlacement,
@@ -11,11 +12,13 @@ from quire.artifacts import (
     SingletonFilePlacement,
     TemplateFilePlacement,
     UnscannablePlacementError,
+    encode_ref_value,
 )
-from quire.contracts import ContractManifest, ContractManifestError, check_contract_manifest
+from quire.contracts import ContractEntry, ContractManifest, ContractManifestError, check_contract_manifest
+from quire.family_store import DocumentFamilyStore
 from quire.git_store import GitGcReport, GitStore, GitStorePolicy
 from quire.refs import RefName, single_field_ref_type, singleton_ref_type
-from quire.notes import NotesRef
+from quire.notes import NotesRef, read_git_note, write_git_note
 from quire.references import (
     AmbiguousReferenceError,
     CrossFamilyReferenceIndex,
@@ -23,9 +26,12 @@ from quire.references import (
     ForeignKeyValidationError,
     ReferenceIndex,
     ReferenceResolution,
+    build_reference_lookup,
+    extend_reference_lookup,
+    finalize_reference_lookup,
     validate_foreign_key,
 )
-from quire.tree_path import FilesystemTreePath, GitTreePath, TreePath
+from quire.tree_path import FilesystemTreePath, GitTreePath, TreePath, coerce_tree_path
 from quire.versions import VersionId
 from quire.families import (
     BoundFamily,
@@ -41,13 +47,16 @@ from quire.hashing import canonical_json_bytes, canonical_json_sha256
 __all__ = [
     "AmbiguousReferenceError",
     "ArtifactFamily",
+    "ArtifactHandle",
     "BranchPlacement",
     "BoundFamily",
     "BoundFamilyRegistry",
     "BoundFamilyTransaction",
+    "ContractEntry",
     "ContractManifest",
     "ContractManifestError",
     "CrossFamilyReferenceIndex",
+    "DocumentFamilyStore",
     "FamilyDefinition",
     "FamilyIdentityPolicy",
     "FamilyRegistry",
@@ -75,8 +84,15 @@ __all__ = [
     "canonical_json_bytes",
     "canonical_json_sha256",
     "check_contract_manifest",
+    "coerce_tree_path",
     "documents",
+    "build_reference_lookup",
+    "encode_ref_value",
+    "extend_reference_lookup",
+    "finalize_reference_lookup",
+    "read_git_note",
     "single_field_ref_type",
     "singleton_ref_type",
     "validate_foreign_key",
+    "write_git_note",
 ]
