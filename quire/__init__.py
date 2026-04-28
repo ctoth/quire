bac6266d5d01f71ca418aa25145b0d2529a962fd
Christopher Toth <q.alpha@gmail.com>
2026-04-28 00:44:41 -0600

    Promote quire public surface

--- quire/__init__.py
+++ quire/__init__.py
@@ -1,10 +1,27 @@
 from __future__ import annotations
 
 from quire import documents
-from quire.git_store import GitStore, GitStorePolicy
+from quire.artifacts import (
+    ArtifactFamily,
+    BranchPlacement,
+    FixedFilePlacement,
+    FlatYamlPlacement,
+    HashScatteredYamlPlacement,
+    SingletonFilePlacement,
+    TemplateFilePlacement,
+)
+from quire.contracts import ContractManifest, ContractManifestError, check_contract_manifest
+from quire.git_store import GitGcReport, GitStore, GitStorePolicy
 from quire.refs import RefName, single_field_ref_type, singleton_ref_type
 from quire.notes import NotesRef
-from quire.references import ForeignKeySpec, ReferenceIndex, ReferenceResolution
+from quire.references import (
+    AmbiguousReferenceError,
+    CrossFamilyReferenceIndex,
+    ForeignKeySpec,
+    ReferenceIndex,
+    ReferenceResolution,
+)
+from quire.tree_path import FilesystemTreePath, GitTreePath, TreePath
 from quire.versions import VersionId
 from quire.families import (
     BoundFamily,
@@ -18,23 +35,39 @@ from quire.families import (
 from quire.hashing import canonical_json_bytes, canonical_json_sha256
 
 __all__ = [
+    "AmbiguousReferenceError",
+    "ArtifactFamily",
+    "BranchPlacement",
     "BoundFamily",
     "BoundFamilyRegistry",
     "BoundFamilyTransaction",
+    "ContractManifest",
+    "ContractManifestError",
+    "CrossFamilyReferenceIndex",
     "FamilyDefinition",
     "FamilyIdentityPolicy",
     "FamilyRegistry",
+    "FilesystemTreePath",
+    "FixedFilePlacement",
+    "FlatYamlPlacement",
     "ForeignKeySpec",
+    "GitGcReport",
     "GitStore",
     "GitStorePolicy",
+    "GitTreePath",
+    "HashScatteredYamlPlacement",
     "NotesRef",
     "RefName",
     "ReferenceIndex",
     "ReferenceResolution",
+    "SingletonFilePlacement",
+    "TemplateFilePlacement",
     "TransactionalBoundFamily",
+    "TreePath",
     "VersionId",
     "canonical_json_bytes",
     "canonical_json_sha256",
+    "check_contract_manifest",
     "documents",
     "single_field_ref_type",
     "singleton_ref_type",
